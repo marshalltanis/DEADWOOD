46bda43ced819c6d5a2db240c4ae1c01237e62ef
befitsandpiper <befitsandpiper@gmail.com>
2016-02-21 17:49:49 -0800

    Updated Role class & subclasses

--- deadwood/deadwood.java
+++ deadwood/deadwood.java
@@ -44,30 +44,55 @@
       /*add 1 to rehearseCount */
     }
 
-//   public class Dice{
-//     public int roll(){
-//       //int random = Math.random(6,1);
-//       return 0;
-//     }
-//   }
-// 
-//   public abstract class Role{
-//     private int rankReq;
-// 
-//     public abstract int reward() {
-//       /* provide money and credit reward for extras, just credit reward for leads on success.
-//       Also on success, shotsLeft for ActingSet is decremented.
-//       on failure, money for extras, nothing for leads. */
-//       return 0;
-//     }
-// 
-//   }
-//   public class Lead extends Role{
-//     /*see Abstract class Role */
-//   }
-//   public class Extra extends Role{
-//     /*see Abstract class Role */
-//   }
+  public class Dice{
+    public int roll(){
+      //int random = Math.random(6,1);
+      return 0;
+    }
+  }
+
+  public abstract class Role{
+    private int rankReq;
+
+    public abstract void reward() {
+      /* provide money and credit reward for extras, just credit reward for leads on success.
+      Also on success, shotsLeft for ActingSet is decremented.
+      on failure, money for extras, nothing for leads. */
+    }
+
+  }
+  public class Lead extends Role{
+
+    public Lead(int val) {
+      rankReq = val;
+    }
+
+    public void reward() {
+      //reward on success only:s
+      if (Player.act() == true)
+        Player.setCredits(Player.getCredits + 2);
+    }
+    /*see Abstract class Role */
+  }
+  public class Extra extends Role{
+
+    public Extra(int val) {
+      rankReq = val;
+    }
+
+    public void reward() {
+      //reward on success:
+      if (Player.act() == true) {
+        Player.setCredits(Player.getCredits + 1);
+        Player.setDollars(Player.getDollars + 1);
+      //reward on failure:
+      } else {
+        Player.setDollars(Player.getDollars + 1);
+      }
+
+    }
+    /*see Abstract class Role */
+  }
 //   public class Scene implements Day{
 //     private int budget;
 // 
